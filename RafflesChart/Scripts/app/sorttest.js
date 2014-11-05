591db645e07a152455542ef23c224dd9aaaaf344
zarni-aung <zarniaung006@gmail.com>
2014-11-05 14:14:14 +0800

    new files

--- RafflesChart/Scripts/app/sorttest.js
+++ RafflesChart/Scripts/app/sorttest.js
@@ -7,3 +7,38 @@ m.controller('sorttest', ['$scope', '$http', function ($scope, $http) {
     $scope.sortdata = [data1,data2];
    
     }]);
+
+m.directive('orderCtl',function () {
+	return{
+		scope:{
+			param:'=userParam',
+			dir:'='
+		},
+		template: ' {{dir}} <img src={{src}}" />  User param: {{param}}'
+		,
+		link:function (scope,el,attrs) {
+			el.css("background-color","red");
+			scope.dir = "up";
+			//alert(el.html);
+			scope.src="http://placehold.it/50x50/ff3377";
+			//el.text(attrs.valtwo);
+			el.bind('mouseenter', function  () {
+				//el.css("background-color" , "blue");
+			});
+
+			el.bind('click',function  () {	
+			//alert(scope.dir);			
+				scope.dir= scope.dir=="up"?"down":"up";
+				
+			    //el.find('img').attr('src' , 'http://placehold.it/50x150/ff8877');
+				if(scope.dir=="up"){
+				    scope.src = "http://placehold.it/50x150/ff8877";
+				} else {
+				    scope.src = "http://placehold.it/50x150/aa88bb";
+				}
+				scope.$apply();
+			})
+		}
+	};
+});
+	// body...
