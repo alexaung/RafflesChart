12505d52b8d2985775d1294a2b55cca35455d616
zarni-aung <zarniaung006@gmail.com>
2014-11-19 18:00:04 +0800

    blog

---
+++ RafflesChart/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RafflesChart.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public Byte[] Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ RafflesChart/Models/BlogViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RafflesChart.Models
+{
+    public class BlogViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [AllowHtml]
+        public string Content { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
