f52360dd04e95e6be7ed585447cc210a23c8e66d
zarni-aung <zarniaung006@gmail.com>
2014-12-09 11:46:16 +0800

    payment manage

---
+++ RafflesChart/Controllers/ManageSubscriptionController.cs
@@ -0,0 +1,30 @@
+﻿using RafflesChart.Models;
+using RafflesChart.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RafflesChart.Controllers
+{
+    public class ManageSubscriptionController : Controller
+    {
+        // GET: ManageSubscription
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            var vm = new SubscriptionEntryViewModel();
+            return View(vm);
+        }
+
+        public ActionResult List()
+        {
+            var vm = new List<SubscriptionEntryViewModel>();
+            return View(vm);
+        }
+    }
+}
---
+++ RafflesChart/Models/SubscriptionEntryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RafflesChart.ViewModels
+{
+    public class SubscriptionEntryViewModel
+    {
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Detail { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public string Status { get; set; }
+    }
+}
